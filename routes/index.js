b21cd5ac2404c7a0e6785a7470fbe6c690be7fb6
Helge Skogly Holm <helge.holm@gmail.com>
2014-10-09 12:00:32 +0200

    Give a descriptive error message if trying to access DrPublish integration when disabled.

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,9 @@ module.exports = function(controller) {
   if (app.settings.DrPublish) {
     var drPubController = require('./drpublish')(app);
     app.use('/integration/drpublish/', drPubController);
+  } else {
+    app.get('/integration/drpublish*', function(req, res) {
+      res.send(404, 'DrPublish integration not enabled.');
+    });
   }
 }
