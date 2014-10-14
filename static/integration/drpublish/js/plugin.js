92c8cdb8135526267b480a88cb9df67d709d8b0d
Helge Skogly Holm <helge.holm@gmail.com>
2014-10-14 15:49:56 +0200

    Re-auth on plugin page change.

--- static/integration/drpublish/js/plugin.js
+++ static/integration/drpublish/js/plugin.js
@@ -1,5 +1,20 @@
 this.DEBUG = true;
 $(document).ready(function() {
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  var name = getParameterByName('appName');
+  var auth = getParameterByName('auth');
+  var iv = getParameterByName('iv');
+  // register name of the app, sent as a paramter in the iframe url
+  AppAPI.setAppName(name);
+  // authenticate the app, again using variables sent in the iframe url
+  AppAPI.doStandardAuthentication("authenticate?auth=" + auth + "&iv=" + iv);
+
   function getValue() {
     return $('#textInput').val();
   }
