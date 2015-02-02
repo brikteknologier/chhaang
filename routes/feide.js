252737f3241d65d5fb4b9cc543d4c0bc6cf47434
Helge Skogly Holm <helge.holm@gmail.com>
2015-02-02 13:44:48 +0100

    Corrected accidental style bleed from Passport-SAML examples.

--- routes/feide.js
+++ routes/feide.js
@@ -4,32 +4,32 @@ module.exports = function(settings, passport) {
   var controller = Controller();
   controller.app.set('view engine', 'jade');
 
-  controller.define('index', function (req, res) {
+  controller.define('index', function(req, res) {
     res.render('feide/index', settings);
   });
 
   controller.define('login', passport.authenticate('saml', {
-    successRedirect : "/",
-    failureRedirect : "/integration/feide/login"
+    successRedirect: "/",
+    failureRedirect: "/integration/feide/login"
   }));
   
   var passportAuthCallback = passport.authenticate('saml', {
-      failureRedirect : "/integration/feide/login",
-      failureFlash: true
+    failureRedirect: "/integration/feide/login",
+    failureFlash: true
   });
   controller.define('loginCallback', function(req, res, next) {
-      passportAuthCallback(req, res, function() {
-	  res.redirect("/integration/feide/index");
-      });
+    passportAuthCallback(req, res, function onAuthCallbackSucceess() {
+      res.redirect("/integration/feide/index");
+    });
   });
 
-  controller.define('logout', function (req, res) {
+  controller.define('logout', function(req, res) {
     // TODO: Send session invalidation request to Feide IP instead
     req.logout();
     res.redirect('/');
   });
 
-  controller.define('profile', function (req, res) {
+  controller.define('profile', function(req, res) {
     if (req.isAuthenticated()) {
       res.render("feide/profile", {
         user: req.user,
