386cb43af328c3557e60fcc15a1341218246a331
Helge Skogly Holm <helge.holm@gmail.com>
2015-02-05 11:03:45 +0100

    Read site_settings slower if read was successful.

--- site_settings.js
+++ site_settings.js
@@ -3,12 +3,15 @@ var fs = require('fs');
 module.exports = function(app) {
   function updateSiteSettings() {
     app.kvass('/api/site/public_site_settings', function(err, settings) {
-      if (err) return app.log.error("Couldn't get site settings - " + err.toString());
+      if (err) {
+        setTimeout(updateSiteSettings, 10000);
+        return app.log.error("Couldn't get site settings - " + err.toString());
+      }
       if (_.isEqual(app.locals.siteSettings, settings)) return;
       app.locals.siteSettings = settings;
     });
+    setTimeout(updateSiteSettings, 5 * 60 * 1000);
   };
 
   updateSiteSettings();
-  setInterval(updateSiteSettings, 10000);
 };
