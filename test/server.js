f12d386bfcc7bee17ee3f1c043e7b355c8061f95
Helge Skogly Holm <helge.holm@gmail.com>
2015-03-25 15:07:21 +0100

    Added test that indicates response ordering problems.

--- test/server.js
+++ test/server.js
@@ -1,17 +1,36 @@
+var http = require('http');
+
 var assert = require('assert');
 var chhaang = require('../core');
 
+var config = {
+  // log: [{"transport":"console"}]},
+};
+
 describe("server with no extensions enabled", function() {
   var server;
+  var rootUrl;
   
   beforeEach(function(done) {
-    chhaang({}, function(err, createdServer) {
+    chhaang(config, function(err, createdServer) {
       server = createdServer;
+      var location = server.address();
+      rootUrl = 'http://' + location.address + ':' + location.port + '/integration/';
       done(err);
     });
   });
 
-  it("has a test", function() {
+  afterEach(function() {
+    server.close();
+    server = null;
+    rootUrl = null;
+  });
+
+  it("handles requests to root url", function(done) {
     assert(server);
+    http.get(rootUrl, function(res) {
+      assert(res.statusCode >= 200 && res.statusCode < 400);
+      done();
+    });
   });
 });
