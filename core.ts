ab50eb5093519f10308a26dc3725d0dca7d18932
arealmaas <arealmaas@gmail.com>
2021-05-04 20:39:33 +0200

    upsert user when authenticated

--- core.ts
+++ core.ts
@@ -21,7 +21,6 @@ const packageJson = require('./package.json');
 var server = (module.exports = http.createServer());
 
 module.exports = async function init(config, callback) {
-  console.log('le config', config);
   var controller = Controller();
   var app = controller.app;
 
@@ -61,66 +60,31 @@ module.exports = async function init(config, callback) {
     passport.use('saml', strategy);
   }
 
-  // todo: only allow either Feide or OpenID?
-
   if (config.OpenID) {
-    // todo: maybe one per provider here?
-
-    const BRIK_AD_CLIENT_ID = 'ffc4b888-227f-4730-9df0-754534fe4b43';
-    const BRIK_AD_TENANT_ID = '0ceb80d1-08c3-4e59-93c7-d029989b96bd';
-    const BRIK_AD_CLIENT_SECRET = 'C97w6g_gSSpie_uf.as5Zlpnir3r64Oni-';
-
-    const BRIK_FEIDE_ID = 'd8d0f6d4-336f-43c9-af5c-7f434dda4c6a';
-    const BRIK_FEIDE_CLIENT_SECRET = '539be99e-3609-467d-b9c0-0efb74b5ee07';
-
-    const microsoftOpenIDURL = `https://login.microsoftonline.com/${BRIK_AD_TENANT_ID}/v2.0`;
-    const microsoftDiscoveryURL = `https://login.microsoftonline.com/${BRIK_AD_TENANT_ID}/v2.0/.well-known/openid-configuration`;
-
-    const feideDiscoveryURL = `https://auth.dataporten.no/.well-known/openid-configuration`;
-
     try {
-      // const issuer = await Issuer.discover(microsoftOpenIDURL);
-      const issuer = await Issuer.discover(feideDiscoveryURL);
-      // todo: can we make this generic? Or maybe that defeats the purpose..
-      // todo: to make it generic, provide a URL and a discovery URL? 🤔
+      const issuer = await Issuer.discover(config.OpenID.discoveryURL);
 
       const issuerOptions: ClientMetadata = {
-        // client_id: BRIK_AD_CLIENT_ID,
-        client_id: BRIK_FEIDE_ID,
-        // client_secret: BRIK_AD_CLIENT_SECRET,
-        client_secret: BRIK_FEIDE_CLIENT_SECRET,
+        client_id: config.OpenID.clientId,
+        client_secret: config.OpenID.clientSecret,
         redirect_uris: [
           'http://localhost:6006/integration/open-id/login/callback',
         ],
-        // scope:["profile"],
+        scope: ['profile'],
         response_types: ['code'],
-        // id_token_signed_response_alg (default "RS256")
       };
 
-      console.log(issuerOptions);
-      /* const client = new issuer.Client({
-        ...issuerOptions,
-        registration_endpoint: `https://login.microsoftonline.com/${BRIK_TENANT_ID}/v2.0/clients`,
-      }); */
-
-      // TODO: Prøv uten passport....!
       const client = new issuer.Client({
         ...issuerOptions,
-        // registration_endpoint: `https://login.microsoftonline.com/${BRIK_TENANT_ID}/v2.0/clients`,
       });
-      // client.grant
-      console.log(client.authorizationUrl());
+
+      // todo: when we need the access token etc to access third party APIs: https://github.com/panva/node-openid-client#authorization-code-flow
       const strategy = new OpenIDStrategy(
         {
           client,
         },
         (tokenset, userInfo, next) => {
-          // todo: do anything with userInfo here?
-          console.log('tokenset', tokenset);
-          console.log('userInfo', userInfo);
-          /* app.log.info(JSON.stringify(tokenset));
-          app.log.info(JSON.stringify(userInfo)); */
-          next(null, tokenset.claims());
+          next(null, userInfo);
         }
       );
 
@@ -132,34 +96,26 @@ module.exports = async function init(config, callback) {
 
       passport.use('open-id', strategy);
     } catch (err) {
-      // todo: use proper logging here
-      console.log('error in the system');
-      console.log(err);
       app.log.error('Failed to initialize Open ID', err);
       callback && callback(err);
       return;
     }
 
-    // todo: when we need the access token etc to access third party APIs: https://github.com/panva/node-openid-client#authorization-code-flow
-
     passport.serializeUser(function (user, done) {
-      console.log('serializeUser', user);
       done(null, user);
     });
     passport.deserializeUser(function (user, done) {
-      console.log('deserializeUser', user);
       done(null, user);
     });
   }
 
-  // app.use(express.urlencoded({ extended: false }));
   app.use(
     session({
       secret: 'sudo apt-get install pants',
       // https://github.com/expressjs/session#resave
-      // resave: false,
+      resave: false,
       // https://github.com/expressjs/session#saveuninitialized
-      // saveUninitialized: true,
+      saveUninitialized: true,
     })
   );
   app.use(passport.initialize());
--- routes/open-id.ts
+++ routes/open-id.ts
@@ -7,6 +7,16 @@ var async = require('async');
 var seraphInit = require('seraph');
 import { PassportStatic } from 'passport';
 
+export interface IOpenIDUser {
+  sub: string;
+  name: string;
+  email: string;
+  email_verified: boolean;
+  picture: string;
+  // custom properties per open id provider
+  [key: string]: any;
+}
+
 module.exports = function (app, passport: PassportStatic) {
   var REDIRECT_COOKIE = 'redirectAfterLogin';
   var defaultRedirect = '/';
@@ -42,44 +52,14 @@ module.exports = function (app, passport: PassportStatic) {
   }
 
   // Return falsy if access allowed, or error string if disallowed
-  function accessDisallowed(idpUser) {
+  function accessDisallowed(openIDUser: IOpenIDUser) {
+    if (!openIDUser.sub) {
+      return 'ID is required';
+    }
+    if (!openIDUser.email) {
+      return 'Email is required';
+    }
     return false;
-    // todo: implement
-    /* function checkString(field, requiredValue) {
-      if (idpUser[field] != requiredValue)
-        return (
-          field +
-          '="' +
-          idpUser[field] +
-          '", but must be "' +
-          requiredValue +
-          '"'
-        );
-    } */
-    /* function checkList(field, requiredValue) {
-      var error =
-        field + '="' + idpUser[field] + '", is missing "' + requiredValue + '"';
-      (idpUser[field] || '').split(',').forEach(function (value) {
-        if (value == requiredValue) error = null;
-      });
-      return error;
-    } */
-    /* var ar = app.config.Feide.accessRequirement || {};
-    app.log.info('checking against access rules: ' + JSON.stringify(ar));
-    for (var field in ar) {
-      var requirement = ar[field];
-
-      var error = null;
-
-      if (requirement.type == 'list')
-        error = checkList(field, requirement.value);
-      else error = checkString(field, requirement.value);
-
-      if (error) {
-        app.log.info('access rules check failed: ' + error);
-        return error;
-      }
-    } */
   }
 
   function invalidateAuthKey(req, res) {
@@ -105,39 +85,8 @@ module.exports = function (app, passport: PassportStatic) {
 
   const passportAuth = passport.authenticate('open-id', {
     failureRedirect: '/integration/open-id/failure',
-    successRedirect: '/integration/open-id/success',
     failureFlash: true,
   });
-
-  // todo: separate passport auth logout?
-  /* var passportAuthLogout = passport('saml', {
-    samlFallback: 'logout-request',
-  }); */
-
-
-  /* todo: feide info hier:
-  https://repl.it/@afell/javascript-login-example
-  https://github.com/jhellan/example3
-
-  For de/oss som bruker C# (.NET Core), kan OpenID konfigueres slik:
-
-        .AddOpenIdConnect("oidc", options =>{
-                options.RequireHttpsMetadata = false;
-                options.MetadataAddress = "https://auth.dataporten.no/.well-known/openid-configuration";
-                options.ClientId = "xxxxxxx";
-                options.ClientSecret = "xxxxxxxxx";
-                options.ResponseType = "code";
-                options.SignedOutRedirectUri = "/home";
-                options.SaveTokens = true;
-                options.GetClaimsFromUserInfoEndpoint = true;
-                options.Scope.Add("profile");
-                options.Scope.Add("openid");
-        }
-
-
-
-
-  */
 
   controller.define('login', function (req, res) {
     var reqUrl = url.parse(req.url, true);
@@ -148,23 +97,16 @@ module.exports = function (app, passport: PassportStatic) {
   });
 
   controller.define('loginCallback', function (req, res) {
-    console.log('loginCallback req.query', req.query);
-    console.log('loginCallback req.body', req.body);
-    function locallyAuthenticateOpenIdUser(idpUser) {
-      // todo: remove this log, because we don't wanna log this result?
-      console.log('locallyAuthenticateOpenIdUser()');
+    function locallyAuthenticateOpenIdUser(openIDUser: IOpenIDUser) {
       app.log.info(
-        'Open ID auth login callback success: ' + JSON.stringify(idpUser)
+        'Open ID auth login callback success: ' + openIDUser && openIDUser.sub
       );
 
-      var reason = accessDisallowed(idpUser);
+      var reason = accessDisallowed(openIDUser);
       if (reason) {
         res.render('open-id/accessRequirementFailed', {
           reason: reason,
-          userName:
-            req.user.displayName ||
-            req.user.givenName + ' ' + req.user.sn ||
-            'Your Open ID user',
+          userName: req.user.name || req.user.sub || 'Your Open ID user',
           json: {
             reason: JSON.stringify(reason),
             user: JSON.stringify(req.user),
@@ -172,23 +114,11 @@ module.exports = function (app, passport: PassportStatic) {
         });
         return;
       }
-      console.log('idp-user', idpUser);
 
-      // `eduPersonTargetedID` is available in an organization's Feide system.
-      // `uid` is available in Feide OpenIdP.
-      var idpId = idpUser.eduPersonTargetedID || idpUser.uid;
-      var translatedUser = {
-        // `displayName` is the user's preferred display name, but not always available
-        // `givenName` and `sn` are first and last names, but not always available
-        // `cn` is the user's local username, but not always available
-        name:
-          idpUser.displayName ||
-          idpUser.givenName + ' ' + idpUser.sn ||
-          idpUser.cn ||
-          idpId,
-        // `mail` is available in an organization's Feide system
-        // `email` is available in Feide OpenIdp.
-        email: idpUser.mail || idpUser.email || '',
+      const openIDUserId = openIDUser.sub;
+      const translatedUser = {
+        name: openIDUser.name || openIDUserId,
+        email: openIDUser.email || '',
       };
 
       async.waterfall(
@@ -197,7 +127,7 @@ module.exports = function (app, passport: PassportStatic) {
           function (User, cb) {
             User.createOrUpdateFromIdentityProvider(
               'open-id',
-              idpId,
+              openIDUserId,
               translatedUser,
               cb
             );
@@ -207,7 +137,7 @@ module.exports = function (app, passport: PassportStatic) {
           if (err) {
             app.log.error(
               "Error creating or updating Open ID user '" +
-                idpId +
+                openIDUserId +
                 "': " +
                 JSON.stringify(err)
             );
@@ -227,17 +157,13 @@ module.exports = function (app, passport: PassportStatic) {
     }
 
     passportAuth(req, res, function onAuthCallbackSuccess() {
-      console.log('callback???');
       if (!req.user) {
         app.log.error('onAuthCallbackSuccess() - No user found on response');
         res.send(500, 'User not found on response.');
       }
-      // todo: wat? 👇
 
       try {
-        console.log('trying to locally authenticate open id user')
         locallyAuthenticateOpenIdUser(req.user);
-        console.log('finished trying to locally authenticate open id user')
       } catch (err) {
         app.log.error(err);
         res.send(500, 'Error handling authentication');
@@ -246,16 +172,8 @@ module.exports = function (app, passport: PassportStatic) {
   });
 
   controller.define('logout', function (req, res) {
-    if (!req.user) {
-      // Not logged in via Feide, just invalidate authKey if present
-      return invalidateAuthKey(req, res);
-    }
-
-    // todo: sign out of open-id?
-    /* passportAuthLogout(req, res, function onLogoutDone() {
-      app.log.info('SAML logout processed');
-      res.redirect('/');
-    }); */
+    // todo: revoke access tokens
+    return invalidateAuthKey(req, res);
   });
 
   controller.define('logoutCallback', invalidateAuthKey);
